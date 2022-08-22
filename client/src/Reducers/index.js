63174756f008059a0c35ec2e64f3c0b32dff7683
zeru15 <zeruf5@gmail.com>
2022-08-23 02:06:58 +0300

    auth reducer

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import ItemReducer from './ItemReducer';
+import ErrorReducer from './ErrorReducer';
+import AuthReducer from './AuthReducer';
 
 export default combineReducers({
-    item: ItemReducer
+    item: ItemReducer,
+    error: ErrorReducer,
+    auth: AuthReducer
 })
